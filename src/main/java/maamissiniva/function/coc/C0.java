a48c370985e1b5670cb48493c37bda5db8883395
Jean Verdier <verdier.jean@gmail.com>
2022-06-14 12:38:03 +0200

    Added C0 and T0.

---
+++ src/main/java/maamissiniva/function/coc/C0.java
@@ -0,0 +1,5 @@
+package maamissiniva.function.coc;
+
+public interface C0 {
+	void accept() throws Exception;
+}
---
+++ src/main/java/maamissiniva/function/coc/T0.java
@@ -0,0 +1,9 @@
+package maamissiniva.function.coc;
+
+public class T0 {
+
+	public static final T0 instance = new T0();
+	
+	private T0() {}
+	
+}
